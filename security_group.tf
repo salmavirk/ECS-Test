a1e10dd9aee912c3155d5a7108eaf04df72d27dc
Salma VIrk <salma@scalefactory.com>
2019-09-24 22:59:26 +0100

    Security group for ASG

---
+++ security_group.tf
@@ -0,0 +1,24 @@
+resource "aws_security_group" "allow-ssh" {
+  vpc_id = "${aws_vpc.ecs.id}"
+  name = "allow-ssh"
+  description = "security group that allows ssh and all egress traffic"
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    terraform = "true"
+    Name      = "allow_ssh"
+  }
+}
