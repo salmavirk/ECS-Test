b2e831e845ccca0dbdc877d538acfe8d4e89b1cc
Salma VIrk <salma@scalefactory.com>
2019-09-26 10:00:36 +0100

    Adding security groups

--- security_group.tf
+++ security_group.tf
@@ -1,12 +1,13 @@
-resource "aws_security_group" "allow-ssh" {
+#EC2 security group
+resource "aws_security_group" "website_allow_ssh" {
   vpc_id = "${aws_vpc.ecs.id}"
-  name = "allow-ssh"
+  name = "allow_ssh"
   description = "security group that allows ssh and all egress traffic"
 
-  egress {
-    from_port = 0
-    to_port = 0
-    protocol = "-1"
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
@@ -19,6 +20,29 @@ resource "aws_security_group" "allow-ssh" {
 
   tags = {
     terraform = "true"
-    Name      = "allow_ssh"
+    Name      = "allow ssh"
+  }
+}
+
+## Security Group for ELB
+resource "aws_security_group" "elb" {
+  vpc_id = "${aws_vpc.ecs.id}"
+  name = "terraform-example-elb"
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    terraform = "true"
+    Name      = "allow all traffi"
   }
 }
