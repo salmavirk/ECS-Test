deac720af236dc20a766c227a1b6ce910001c857
Salma VIrk <salma@scalefactory.com>
2019-09-26 09:57:58 +0100

    Renaming components

--- autoscaling_policy.tf
+++ autoscaling_policy.tf
@@ -1,28 +1,38 @@
-resource "aws_launch_configuration" "example-launchconfig" {
-  name_prefix = "example-launchconfig"
+resource "aws_launch_configuration" "website_launchconfig" {
+  name_prefix = "website-launchconfig"
+  associate_public_ip_address = true
   image_id = "${lookup(var.ami_map, var.aws_region)}"
   instance_type = "t2.micro"
+  user_data = "${file("user-data.sh")}"
   key_name = "${aws_key_pair.terraform_ec2_key.key_name}"
-  security_groups = ["${aws_security_group.allow-ssh.id}"]
+  security_groups = ["${aws_security_group.website_allow_ssh.id}"]
 }
 
 resource "aws_key_pair" "terraform_ec2_key" {
   key_name = "terraform_ec2_key"
   public_key = "${file("terraform_ec2_key.pub")}"
 }
-resource "aws_autoscaling_group" "example-autoscaling" {
-  name = "example-autoscaling"
-  vpc_zone_identifier = ["${aws_subnet.ecs_subnet_a.id}", "${aws_subnet.ecs_subnet_b.id}"]
-  launch_configuration = "${aws_launch_configuration.example-launchconfig.name}"
+resource "aws_autoscaling_group" "website_autoscaling" {
+  name = "website-autoscaling"
+  vpc_zone_identifier = ["${aws_subnet.ecs_subnet_a.id}", "${aws_subnet.ecs_subnet_b.id}", "${aws_subnet.ecs_subnet_c.id}"]
+  launch_configuration = "${aws_launch_configuration.website_launchconfig.name}"
   min_size = 1
-  max_size = 2
+  max_size = 3
   health_check_grace_period = 300
-  health_check_type = "EC2"
+  load_balancers = ["${aws_elb.website_lb.name}"]
+  health_check_type = "ELB"
   force_delete = true
 
-  tag {
-    key = "Name"
-    value = "ec2 instance"
-    propagate_at_launch = true
-  }
+  tags = [
+    {
+      key                 = "Name"
+      value               = "website instance"
+      propagate_at_launch = true
+    },
+    {
+      key                 = "terraform"
+      value               = "true"
+      propagate_at_launch = true
+    },
+  ]
 }
